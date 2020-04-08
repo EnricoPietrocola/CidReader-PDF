f461328174b288f8b437aee64ca2f6741a6c295a
ePietrocola <enricopietrocola@gmail.com>
2020-04-08 10:54:14 +0200

    Cleaned code

--- CidReaderPDF/mupdf-lib/src/main/java/com/artifex/mupdf/mini/SyncUDP.java
+++ CidReaderPDF/mupdf-lib/src/main/java/com/artifex/mupdf/mini/SyncUDP.java
@@ -19,14 +19,6 @@ public class SyncUDP extends AsyncTask<Object, String, String> {
             int time = (int)params[0];
             String ip = (String)params[1];
             ArrayList<PaintView> paintViews = (ArrayList<PaintView>) params[2];
-            /*int secs = 0;
-            for(int i = 0; i < paintViews.size(); i++){
-
-                Thread.sleep(time);
-
-                resp = "Slept for " + params[0] + " seconds";
-                Log.i("CID", "DOING " + resp + " secs " + secs);
-            }*/
 
             for (int i = 0; i < paintViews.size(); i++){
                 //create remote paintview with IP HERE
@@ -34,24 +26,9 @@ public class SyncUDP extends AsyncTask<Object, String, String> {
 
                 //get pages
                 for(int j = 0; j < paintViews.get(i).actionPages.size(); j++) {
-                    //draw stuff paintViews.get(i).actionPages.get(j)
-
                     //get page
                     for(int l = 0; l < paintViews.get(i).actionPages.get(j).size(); l++){
-
-
-                        Log.i("CID", "SyncAction " + paintViews.get(i).actionPages.get(j).get(l));
-                        //String[] parsedActionMessage = parseAction(paintViews.get(i).actionPages.get(j).get(l));
-								/*for(int y = 0; y < parsedStuff.length; y++){
-									Log.i("CID", parsedStuff[y]);
-								}*/
-                        /*String action = parsedActionMessage[0];
-                        int pageNumber = Integer.parseInt(parsedActionMessage[1]);
-                        float x = Float.parseFloat(parsedActionMessage[2]);
-                        float y = Float.parseFloat(parsedActionMessage[3]);
-                        int strokeWidth = Integer.parseInt(parsedActionMessage[4]);
-                        int color = Integer.parseInt(parsedActionMessage[5]);
-                        boolean isTrail = Boolean.parseBoolean(parsedActionMessage[6]);*/
+                        //Log.i("CID", "SyncAction " + paintViews.get(i).actionPages.get(j).get(l));
                         Thread.sleep(time);
                         syncDrawOnScreen(paintViews.get(i).actionPages.get(j).get(l));
 
@@ -71,11 +48,10 @@ public class SyncUDP extends AsyncTask<Object, String, String> {
     }
 
 
-    //@Override
+    @Override
     protected void onPostExecute(String result) {
         // execution of result of Long time consuming operation
-        //progressDialog.dismiss();
-        //finalResult.setText(result);
+
     }
 
 
@@ -87,42 +63,7 @@ public class SyncUDP extends AsyncTask<Object, String, String> {
     @Override
     protected void onProgressUpdate(String... text) {
     }
-
-    public void syncAnnotationsToIp(String ip, ArrayList<PaintView> paintViews){
-
-
-
-        for (int i = 0; i < paintViews.size(); i++){
-            //create remote paintview with IP HERE
-            Log.i("CID", "FOR IP " + paintViews.get(i).ipAddress);
-
-            //get pages
-            for(int j = 0; j < paintViews.get(i).actionPages.size(); j++) {
-                //draw stuff paintViews.get(i).actionPages.get(j)
-
-                //get page
-                for(int l = 0; l < paintViews.get(i).actionPages.get(j).size(); l++){
-
-
-                    Log.i("CID", "SyncAction " + paintViews.get(i).actionPages.get(j).get(l));
-                    //String[] parsedActionMessage = parseAction(paintViews.get(i).actionPages.get(j).get(l));
-								/*for(int y = 0; y < parsedStuff.length; y++){
-									Log.i("CID", parsedStuff[y]);
-								}*/
-					/*String action = parsedActionMessage[0];
-					int pageNumber = Integer.parseInt(parsedActionMessage[1]);
-					float x = Float.parseFloat(parsedActionMessage[2]);
-					float y = Float.parseFloat(parsedActionMessage[3]);
-					int strokeWidth = Integer.parseInt(parsedActionMessage[4]);
-					int color = Integer.parseInt(parsedActionMessage[5]);
-					boolean isTrail = Boolean.parseBoolean(parsedActionMessage[6]);*/
-                    syncDrawOnScreen(paintViews.get(i).actionPages.get(j).get(l));
-
-                }
-            }
-        }
-    }
-
+    
     private void syncDrawOnScreen(String action){
         UDP_Client udpClient = new UDP_Client();
         udpClient.addr = DocumentActivity.ipTargetAddress;
